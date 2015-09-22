1d7da0a1f3afc5ce60cf54b3e4755897d725c663
chanwook <chanwook.god@gmail.com>
2015-09-23 02:31:17 +0900

    add sample code for View and ScriptEngine instance mechanism.

---
+++ src/main/java/spring/script/template/InspectViewFlowController.java
@@ -0,0 +1,23 @@
+package spring.script.template;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author chanwook
+ */
+@Controller
+public class InspectViewFlowController {
+
+    // STV instance - 1?
+    @RequestMapping("/dust/view1")
+    public String view1() {
+        return "template1";
+    }
+
+    // STV instance - 2?
+    @RequestMapping("/dust/view2")
+    public String view2() {
+        return "template2";
+    }
+}
--- src/main/resources/dust/dust.js
+++ src/main/resources/dust/dust.js
@@ -1,12 +1,13 @@
 var templates = {};
 
 function render(template, model, url) {
+
     if (templates[url] === undefined) {
         var compiled = dust.compile(template, url); // compile
         dust.loadSource(compiled); // load
     }
 
-    // render
+    // render. dust is basically asynchronously then this code has potentially issue with synchronous..
     var res;
     dust.render(url, model, function (err, data) {
         if (err) {
